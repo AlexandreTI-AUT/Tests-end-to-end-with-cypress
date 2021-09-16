40a96c0aeff98c48387756ae7f1f892dc758c387
Alexandre Silva <alexix.dasilva@gmail.com>
2021-09-16 18:34:00 -0300

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,22 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
+        "es2021": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
     "parserOptions": {
-        "ecmaVersion": 12
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
     },
+    "plugins": [
+        "react"
+    ],
     "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "no-trailing-spaces": [
-            "error"
-        ]
     }
 };
