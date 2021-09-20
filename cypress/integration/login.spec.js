ecff1ff05e46b3f2b70cf8ec6fe7467670c271ef
Alexandre <alexix.dasilva@gmail.com>
2021-09-20 18:33:29 -0300

    Format Doc

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,10 +2,10 @@ it('successfully logs in', () => {
     cy.intercept('GET', '**/notes').as('getNotes')
 
     cy.login(
-    Cypress.env('USER_EMAIL'),
-    Cypress.env('USER_PASSWORD'),
-    { cacheSession: false }
+        Cypress.env('USER_EMAIL'),
+        Cypress.env('USER_PASSWORD'),
+        { cacheSession: false }
     )
 
     cy.wait('@getNotes')
-}) 
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,31 +1,31 @@
 Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
-    cy.visit('/signup')
-    cy.get('#email').type(email)
-    cy.get('#password').type(password, { log: false })
-    cy.get('#confirmPassword').type(password, { log: false })
-    cy.contains('button', 'Signup').click()
-    cy.get('#confirmationCode').should('be.visible')
-  })
+  cy.visit('/signup')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#confirmPassword').type(password, { log: false })
+  cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
+})
 
-  Cypress.Commands.add('login', (
-    username = Cypress.env('USER_EMAIL'),
-    password = Cypress.env('USER_PASSWORD'),
-    { cacheSession = true } = {}
-  ) => {
-    const login = () => {
-      cy.visit('/login')
-      cy.get('#email').type(username)
-      cy.get('#password').type(password, { log: false })
-      cy.contains('button', 'Login').click()
-      cy.contains('h1', 'Your Notes').should('be.visible')
-    }
-  
-    if (cacheSession) {
-      cy.session([username, password], login)
-    } else {
-      login()
-    }
-  })
+Cypress.Commands.add('login', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD'),
+  { cacheSession = true } = {}
+) => {
+  const login = () => {
+    cy.visit('/login')
+    cy.get('#email').type(username)
+    cy.get('#password').type(password, { log: false })
+    cy.contains('button', 'Login').click()
+    cy.contains('h1', 'Your Notes').should('be.visible')
+  }
+
+  if (cacheSession) {
+    cy.session([username, password], login)
+  } else {
+    login()
+  }
+})
 
 
 const attachFileHandler = () => cy.get('#file').attachFile('example.json')
